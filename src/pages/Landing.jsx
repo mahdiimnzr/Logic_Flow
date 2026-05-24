4ea46f81161f5ecc940a6a16ca5412b25a18b895
Mahdi <mahdi7813nazarzadeh@gmail.com>
2026-05-25 03:12:37 +0330

    feature/landing: Adding Button And Form Input Components!

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,44 @@
+import { Formik, Form } from "formik";
 import Button from "../components/atoms/Button/Button";
+import FormInput from "../components/molecules/Inputs/FormInput";
+import * as Yup from "yup";
+import EmailIcon from "../core/icons/EmailIcon";
+import CheckBox from "../components/molecules/Inputs/CheckBox";
+
+const validationSchema = Yup.object({
+  text: Yup.string().required("Please Fill The Box!"),
+});
 
 const Landing = () => {
   return (
     <div className={``}>
-      <Button color={"authBtn"} className={`w-full h-15`}>
-        Salam
+      <Button color={"authBtn"} className={`w-full h-15 font-bold`}>
+        پایتون + ماینکرفت = یادگیری برنامه‌ نویسی با بازی!
       </Button>
+      <Formik
+        validationSchema={validationSchema}
+        initialValues={{
+          text: "",
+          c1: "",
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ errors }) => (
+          <Form>
+            <FormInput
+              icon={<EmailIcon />}
+              error={errors.text}
+              name={"text"}
+              type={"text"}
+              placeholder={"ایمیل یا شماره تماس"}
+            />
+            <CheckBox />
+            <button type="submit" className="text-default-light">
+              submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
--- src/templates/MainLayout.jsx
+++ src/templates/MainLayout.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
-  )
-}
+    <div>
+      <Outlet />
+    </div>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
