1f1662c9753249b7c0a70191f74619d254b14eec
Mahdi <mahdi7813nazarzadeh@gmail.com>
2026-05-24 17:45:05 +0330

    feat/landing: Add Some Themes And Icon Components And A Button Component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,13 @@
+import Button from "../components/atoms/Button/Button";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className={``}>
+      <Button color={"authBtn"} className={`w-full h-15`}>
+        Salam
+      </Button>
+    </div>
+  );
 };
 
 export default Landing;
