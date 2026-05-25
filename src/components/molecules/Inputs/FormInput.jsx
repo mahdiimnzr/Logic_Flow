b648acf2d579dda884c1ecd172f293287c392ce8
Mahdis <mahdismehnatkesh@gmail.com>
2026-05-26 01:06:05 +0330

    feature/auth: add login step1

--- src/components/molecules/Inputs/FormInput.jsx
+++ src/components/molecules/Inputs/FormInput.jsx
@@ -34,7 +34,7 @@ const FormInput = ({ placeholder, name, error, icon, type = "text" }) => {
       <ErrorMessage
         name="text"
         component={"span"}
-        className="text-red-error text-[14px] font-normal"
+        className="text-red-error text-[14px] font-normal mt-2"
       />
     </div>
   );
