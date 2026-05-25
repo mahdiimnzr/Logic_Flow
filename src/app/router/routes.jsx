0a1bcb75aed205978a387e83834f7d4848ebc390
Mahdis <mahdismehnatkesh@gmail.com>
2026-05-25 14:43:09 +0330

    feature/auth: Add login for bug fix

--- src/app/router/routes.jsx
+++ src/app/router/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import mainRoute from "./main.route";
+import authRoute from "./auth.route";
 
-const router = createBrowserRouter([...mainRoute]);
+const router = createBrowserRouter([...mainRoute, ...authRoute]);
 
 export default router;
---
+++ src/components/auth/login/Step1.jsx
@@ -0,0 +1,11 @@
+import FormInput from "../../molecules/Inputs/FormInput";
+
+const Step1 = () => {
+  return (
+    <div>
+      <FormInput />
+    </div>
+  );
+};
+
+export default Step1;
