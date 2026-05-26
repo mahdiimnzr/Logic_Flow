e9cbec7fad037627068ea54527acec5ac2b80a10
Mahdis <mahdismehnatkesh@gmail.com>
2026-05-27 01:04:07 +0330

    feature/auth:login page

--- src/templates/AuthLayout.jsx
+++ src/templates/AuthLayout.jsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
-    <div className={`min-h-screen pt-30 `}>
-      <div className={` bg-default-light xl:w-9/10  mx-auto rounded-[60px] `}>
+    <div className={`min-h-screen xl:pt-20 md:pt-30 pt-10 `}>
+      <div className={`bg-default-light w-9/10  mx-auto rounded-[60px]`}>
         <Outlet />
       </div>
     </div>
