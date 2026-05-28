6df85d4c89f2a71948448f04d73d809b6274c184
Mahdi <mahdi7813nazarzadeh@gmail.com>
2026-05-29 03:09:51 +0330

    feature/landing: add CoursesSection with Api

--- src/components/molecules/Cards/Card.jsx
+++ src/components/molecules/Cards/Card.jsx
@@ -9,7 +9,7 @@ import TechnologyCardIcon from "../../../core/icons/TechnologyCardIcon";
 import formatPrice from "../../../core/utils/formatPrice";
 
 const Card = (props) => {
-  const { isCourseCard = false } = props;
+  const { isCourseCard = false, isFavorite = false } = props;
   return (
     <div
       dir="rtl"
@@ -18,7 +18,7 @@ const Card = (props) => {
       <div
         className={`absolute z-10 right-4 top-4 content-center bg-default-black/25 size-10 rounded-full cursor-pointer`}
       >
-        <FavoriteIcon isFavorite={false} className={`mx-auto`} />
+        <FavoriteIcon isFavorite={isFavorite} className={`mx-auto`} />
       </div>
       <Link className={`h-60 group content-center block relative`}>
         <img
