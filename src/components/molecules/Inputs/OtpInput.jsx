996bfd68d1471b3e8d8c1fe96999d384319052c1
Mahdi <mahdi7813nazarzadeh@gmail.com>
2026-05-31 02:48:45 +0330

    feature/auth: add responsive design for login and register

--- src/components/molecules/Inputs/OtpInput.jsx
+++ src/components/molecules/Inputs/OtpInput.jsx
@@ -20,7 +20,7 @@ const OtpInput = ({ otp, setOtpValue, otpValue, error }) => {
       <InputOTPGroup className={`flex! items-center! justify-between! w-full!`}>
         {otp?.map((value, index) => (
           <InputOTPSlot
-            className={`lg:text-[20px] md:text-base text-[18px] box-border xl:size-15 lg:size-13 md:size-10 sm:size-15 size-12 border-transparent border-2 outline-none ring-0 shadow-none bg-background-default xl:rounded-[15px] md:rounded-[10px] sm:rounded-[15px] rounded-[12px] data-[active=true]:transform-[scale(1.25)] ${otpValue[index] && "border-green-primary"} ${error && !otpValue[index] && "border-red-error"}`}
+            className={`lg:text-[20px] md:text-base text-[18px] box-border xl:size-15 lg:size-13 md:size-10 sm:size-15 size-9 border-transparent border-2 outline-none ring-0 shadow-none bg-background-default xl:rounded-[15px] md:rounded-[10px] sm:rounded-[15px] rounded-[10px] data-[active=true]:transform-[scale(1.25)] ${otpValue[index] && "border-green-primary"} ${error && !otpValue[index] && "border-red-error"}`}
             key={index}
             index={index}
           />
