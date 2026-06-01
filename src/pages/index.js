4af70acdcaa79b68030eabb48a4259fddc5641ab
Mahdi <mahdi7813nazarzadeh@gmail.com>
2026-06-01 17:57:00 +0330

    feature/courseDetail:  add CourseDetail Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ import { lazy } from "react";
 
 export const Landing = lazy(() => import("./Landing"));
 export const CourseDetail = lazy(() => import("./CourseDetail"));
+export const Courses = lazy(() => import("./Courses"));
